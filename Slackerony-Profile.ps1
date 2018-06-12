825a1e4622f788c77ad8dacc41de229210e13ff6
Jonathan Østrup <joos@netcompany.com>
2018-06-12 10:19:41 +0200

    Updated values

--- Slackerony-Profile.ps1
+++ Slackerony-Profile.ps1
@@ -1,22 +1,16 @@
-if (-not(Get-Module -ListAvailable -Name 'posh-git') -or
-    -not(Get-Module -ListAvailable -Name 'posh-docker') -or
-    -not(Get-Module -ListAvailable -Name 'ntfssecurity'))
+if (-not(Get-Module -ListAvailable -Name 'posh-git'))
 {
     Install-Module -Name 'posh-git' -Force -Scope CurrentUser
-    Install-Module -Name 'posh-docker' -Force -Scope CurrentUser
-    Install-Module -Name 'ntfssecurity' -Force -Scope CurrentUser
 }
 
 #Module Imports
 Import-module -Name 'posh-git'
-Import-Module -Name 'posh-docker'
 
 #Custom Module Imports
-Import-Module -Name "$PSScriptRoot\..\WindowsAutomationScripts\Modules\PSFX\PSFX.psm1"
 
 #Custom Aliases
 New-Alias -Name 'dig' -Value 'Resolve-DnsName'
 
 #Console Config
-Set-location -Path 'C:\Users\Jonathan\Documents\GitHub\'
+Set-location -Path "$($ENV:USERPROFILE)\git"
 Start-SshAgent -Quiet
